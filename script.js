6333d040487981125a353cc0adc670d148416304
Jose Barrios <devmusbar@gmail.com>
2023-03-23 10:42:53 -0500

    Agreogo el About y comienzo las skills

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ var typed = new Typed(".typing",{
     backDelay:1000, 
     loop: true
 })
+
+var typed2 = new Typed(".typing-2",{
+    strings: ["FullStack Developer Student"], 
+    typeSpeed:100,
+    backSpeed: 100, 
+    backDelay:1000, 
+    loop: true
+})
