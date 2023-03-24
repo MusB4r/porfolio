ab9614bb1571c539b7e5288dfe4dd32b80696e94
Jose Barrios <devmusbar@gmail.com>
2023-03-23 19:21:58 -0500

    competado todos las sections y agregado scripts a navbar

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var typed = new Typed(".typing",{
     backSpeed: 100, 
     backDelay:1000, 
     loop: true
-})
+});
 
 var typed2 = new Typed(".typing-2",{
     strings: ["FullStack Developer Student"], 
@@ -12,4 +12,13 @@ var typed2 = new Typed(".typing-2",{
     backSpeed: 100, 
     backDelay:1000, 
     loop: true
-})
+});
+
+var typed3 = new Typed(".typing-3",{
+    strings: ["Skills"], 
+    typeSpeed:100,
+    backSpeed: 100, 
+    backDelay:1000, 
+    loop: true
+});
+
